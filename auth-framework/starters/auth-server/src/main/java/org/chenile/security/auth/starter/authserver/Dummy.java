8dfdd69f55aa13c186dc5099656530a211e19423
Raja Shankar Kolluru <raja@thekollurus.com>
2026-06-28 17:56:18 +0530

    Added changes for deployment

---
+++ auth-framework/starters/auth-server/src/main/java/org/chenile/security/auth/starter/authserver/Dummy.java
@@ -0,0 +1,10 @@
+package org.chenile.security.auth.starter.authserver;
+
+/**
+ * The idea here is to keep the java doc happy. So that this can be published into maven central.
+ */
+public class Dummy {
+    public void dummy(){
+
+    }
+}
